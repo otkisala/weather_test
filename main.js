83b22f4445a4b02708afeae2fa50da979432553d
Syffikc <sovex76@gmail.com>
2024-03-02 21:09:03 +0500

    детали

--- main.js
+++ main.js
@@ -1,85 +1,30 @@
-const BUTTONSEARCH = document.querySelector(".search")
-const cityInput = document.querySelector(".inputCity")
-const SERVERURL = 'http://api.openweathermap.org/data/2.5/weather';
-const cityName = document.querySelector(".inputCity").value;
-const APIKEY = '3136a63ccda334b76672d126751488cb';
-const url = `${SERVERURL}?q=${document.querySelector(".inputCity").value}&appid=${APIKEY}`
-const TEMPERATURE = document.querySelector(".temperature")
-const CURRENTCITY = document.querySelector(".currentCity")
-const CURRENTIMG = document.querySelector(".currentImg")
-const CITIES = document.querySelector(".cities")
-const HEART = document.querySelector(".heart")
-const DELETEBUTTON = document.querySelector(".deletDiv")
-const LIPARENT = document.querySelector(".cities")
-const FEELSLIKE = document.querySelector(".feelsLike")
-const SUNRISE = document.querySelector(".sunrise")
-const SUNSET = document.querySelector(".sunset")
-let favorit = []
-
-function weather() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${document.querySelector(".inputCity").value}&appid=3136a63ccda334b76672d126751488cb`)
-        .then(response => {
-            if (response.status === 404 || document.querySelector(".inputCity").value == "") {
-                throw new Error("неправильно набран город")
-            }
-            return response.json()
-        })
-        .then(data => {
-            SUNSET.textContent = `Sunset: ${new Date(data.sys.sunset * 1000).getHours()}:${new Date(data.sys.sunrise).getMinutes()}`
-            SUNRISE.textContent = `Sunrise: ${new Date(data.sys.sunrise * 1000).getHours()}:${new Date(data.sys.sunrise).getMinutes()}`
-            FEELSLIKE.textContent = `Feels like: ${(Math.ceil((data.main.feels_like) - 273))}`
-            TEMPERATURE.textContent = (Math.ceil((data.main.temp) - 273)) + `°`
-            CURRENTCITY.textContent = document.querySelector(".inputCity").value.toLowerCase()
-            CURRENTCITY.textContent = document.querySelector(".inputCity").value[0].toUpperCase() + document.querySelector(".inputCity").value.slice(1);
-            if (data.weather[0].id === 800) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/01d@4x.png"
-            } else if (data.weather[0].id === 801) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/02d@4x.png"
-            } else if (data.weather[0].id === 803 || data.weather[0].id === 804) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/04d@4x.png"
-            } else if (Math.floor(data.weather[0].id / 100) === 7) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/50d@4x.png"
-            } else if (Math.floor(data.weather[0].id / 100) === 6 || data.weather[0].id === 511) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/13d@4x.png"
-            } else if (data.weather[0].id >= 500 && data.weather[0].id <= 504) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/10d@4x.png"
-            } else if (data.weather[0].id >= 520 && data.weather[0].id <= 531) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/09d@4x.png"
-            } else if (Math.floor(data.weather[0].id / 100) === 3) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/09d@4x.png"
-            } else if (Math.floor(data.weather[0].id / 100) === 2) {
-                CURRENTIMG.src = "http://openweathermap.org/img/wn/11d@4x.png"
-            }
-        })
-        .catch(error => console.error(error))
-}
+import { BUTTON_SEARCH, CURRENT_CITY, CITIES, HEART, DELETE_BUTTON, LI_PARENT, favorit, } from "./config.js"
+import { weather } from "./temperatureFetch.js"
 
 function test(e) {
     e.preventDefault();
     weather()
-    redHeart()
     render()
-}
+};
 
 function favorite() {
     try {
-        if (!favorit.includes(CURRENTCITY.textContent)) {
-            favorit.push(CURRENTCITY.textContent)
+        if (!favorit.includes(CURRENT_CITY.textContent)) {
+            favorit.push(CURRENT_CITY.textContent)
             render()
         } else {
             throw new Error("Данный город уже есть")
         }
     } catch (err) {
         console.error(err)
-
     }
-}
+};
 
 function render() {
     while (CITIES.firstChild) {
         CITIES.removeChild(CITIES.firstChild)
     }
-    for (i = 0; i < favorit.length; i++) {
+    for (let i = 0; i < favorit.length; i++) {
         const newLi = document.createElement('li')
         const deletBut = document.createElement('button')
         newLi.textContent = favorit[i]
@@ -90,39 +35,36 @@ function render() {
         deletBut.className = "deletDiv"
     }
     redHeart()
-}
+};
 
 function redHeart() {
     if (!favorit.includes(document.querySelector(".inputCity").value)) {
         HEART.src = "./icons/heart.png"
-        return
+    } else {
+        HEART.src = "./icons/redheart.png"
     }
-    HEART.src = "./icons/redheart.png"
-    // if (favorit.includes(CURRENTCITY.textContent)) {
-    //     HEART.src = "./icons/redheart.png"
-
-    // }
+    // setTimeout(function () {
+    if (favorit.includes(CURRENT_CITY.textContent)) {
+        HEART.src = "./icons/redheart.png"
+    }
+    // }, 1000)
 }
 
 function deleteTest(event) {
     let cityToDelet = event.target.parentNode.textContent.slice(0, -1)
     let IndexCityToDelet = favorit.indexOf(cityToDelet)
     favorit.splice(IndexCityToDelet, 1)
-    redHeart()
     render()
-}
+};
 
 function cityToFind(event) {
     let cityToFind = event.target.textContent.slice(0, -1)
     document.querySelector(".inputCity").value = cityToFind
     weather()
-    redHeart()
     render()
-}
+};
 
-BUTTONSEARCH.addEventListener("click", test)
-BUTTONSEARCH.addEventListener("click", redHeart)
+BUTTON_SEARCH.addEventListener("click", test)
 HEART.addEventListener("click", favorite)
-HEART.addEventListener("click", redHeart)
-DELETEBUTTON.addEventListener("click", deleteTest)
-LIPARENT.addEventListener("click", cityToFind)
+DELETE_BUTTON.addEventListener("click", deleteTest)
+LI_PARENT.addEventListener("click", cityToFind)
---
+++ temperatureFetch.js
@@ -0,0 +1,43 @@
+import { TEMPERATURE, CURRENT_CITY, CURRENT_IMG, FEELS_LIKE, SUNRISE, SUNSET, MORNING_IMG, DAY_IMG, NIGHT_IMG, MORNING_TEMP, MORNING_FEELS, DAY_TEMP, DAY_FEELS, NIGHT_TEMP, NIGHT_FEELS, FAHRENHEIT_TO_CELSIUS_OFFSET } from "./config.js"
+import { obj } from "./weatherVisuals.js";
+
+export function predictions() {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${document.querySelector(".inputCity").value}&appid=3136a63ccda334b76672d126751488cb`)
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            MORNING_TEMP.textContent = `Temperature: ${(Math.ceil((data.list[0].main.temp) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            MORNING_FEELS.textContent = `Feels like:: ${(Math.ceil((data.list[0].main.feels_like) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            DAY_TEMP.textContent = `Temperature: ${(Math.ceil((data.list[1].main.temp) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            DAY_FEELS.textContent = `Feels like:: ${(Math.ceil((data.list[1].main.feels_like) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            NIGHT_TEMP.textContent = `Temperature: ${(Math.ceil((data.list[2].main.temp) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            NIGHT_FEELS.textContent = `Feels like:: ${(Math.ceil((data.list[2].main.feels_like) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            MORNING_IMG.src = obj[data.list[0].weather[0].id]
+            DAY_IMG.src = obj[data.list[1].weather[0].id]
+            NIGHT_IMG.src = obj[data.list[2].weather[0].id]
+        })
+}
+
+export function weather() {
+    if (document.querySelector(".inputCity").value == "") {
+        throw new Error("Пустая строка")
+    } fetch(`http://api.openweathermap.org/data/2.5/weather?q=${document.querySelector(".inputCity").value}&appid=3136a63ccda334b76672d126751488cb`)
+        .then(response => {
+            if (response.status === 404) {
+                throw new Error("Неправильно набран город")
+            }
+            return response.json()
+        })
+        .then(data => {
+            SUNSET.textContent = `Sunset: ${new Date(data.sys.sunset * 1000).getHours()}:${new Date(data.sys.sunrise).getMinutes()}`
+            SUNRISE.textContent = `Sunrise: ${new Date(data.sys.sunrise * 1000).getHours()}:${new Date(data.sys.sunrise).getMinutes()}`
+            FEELS_LIKE.textContent = `Feels like: ${(Math.ceil((data.main.feels_like) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            TEMPERATURE.textContent = `${(Math.ceil((data.main.temp) - FAHRENHEIT_TO_CELSIUS_OFFSET))}`
+            CURRENT_CITY.textContent = document.querySelector(".inputCity").value.toLowerCase()
+            CURRENT_CITY.textContent = document.querySelector(".inputCity").value[0].toUpperCase() + document.querySelector(".inputCity").value.slice(1);
+            CURRENT_IMG.src = obj[data.weather[0].id]
+        })
+        .catch(error => console.error(error))
+        predictions()
+};
